82d2393ffd1757f7390e1d4cdf8c67febec867fe
unknown <wlj@lilanz.local>
2024-01-19 17:37:14 +0800

    'setter'

--- packages/plugins/plugin-setter/src/components/PropsSetter/index.vue
+++ packages/plugins/plugin-setter/src/components/PropsSetter/index.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
 import { useSetterInjection } from '../../context';
+import { NGrid, NGi } from 'naive-ui';
 
 const { meta, schema, setter } = useSetterInjection();
 </script>
 
 <template>
   <div>
-    <div class="px-30px">
-      <component :is="meta.icon" />
-    </div>
+    <n-grid class="px-30px">
+      <n-gi span="5">
+        <component class="max-w-30px max-h-30px" :is="meta.icon" />
+      </n-gi>
+      <n-gi span="10">
+        {{ setter.title }}
+      </n-gi>
+      <n-gi class="text-12px">
+        {{ schema.id }}
+      </n-gi>
+    </n-grid>
   </div>
 </template>
 
